260056ad18d17b66036a9321008fb773a74cb427
Chiheng Li <lichiheng1998@gmail.com>
2018-08-27 12:58:36 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('MongoDB');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -27,6 +29,32 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/1234324
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Validate id using isValid
+    // 404 - send back empty send
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({error: 'Id is invalid'});
+  }
+  // findById
+  // success
+    // if todo - send it back
+    // if no todo - send back 404 with empty body
+  // error
+    // 400 - and send empty body back
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(3000, () => {
