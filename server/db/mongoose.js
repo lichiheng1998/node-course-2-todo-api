43761bd10d041315d26e5175ce2153a2e3ba31fd
Chiheng Li <lichiheng1998@gmail.com>
2018-08-29 12:24:44 -0400

    Setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 // Config the mongoose to use the native promise library
 mongoose.promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
