f92de49c6e5ab63be26b0a56831303f0211aa6ec
Chiheng Li <lichiheng1998@gmail.com>
2018-08-31 18:13:06 -0400

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,59 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// User
-// email - require it - trim it - set type - set min length of 1
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     require: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      require: true
+    },
+    token: {
+      type: String,
+      require: true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+
+   return user.save().then(() => {
+    // return in promise, another promise will be returned.
+    return token;
+  });
+};
+
+// User
+// email - require it - trim it - set type - set min length of 1
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
