56af03e6411ba1b3c5b5ec4bc9fd7e7a4dccee9c
Chiheng Li <lichiheng1998@gmail.com>
2018-09-03 13:59:30 -0400

    Add tests for POST /users/login

--- server/test/seed/seed.js
+++ server/test/seed/seed.js
@@ -18,10 +18,6 @@ const users = [{
   _id: userTwoId,
   email: 'jen@example.com',
   password: 'userTwoPass',
-  tokens: [{
-    access: 'auth',
-    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
-  }]
 }];
 
 const todos = [{
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -229,7 +229,7 @@ describe('POST /users', () => {
             expect(res).toBe(true);
             done();
           });
-        });
+        }).catch((e) => done(e));
       });
   });
 
@@ -253,5 +253,61 @@ describe('POST /users', () => {
       })
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[0].email,
+        password: users[0].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          // POSTing to 'users/login/' causes a token to be pushed on to the the
+          // user's token array in generateAuthToken(). Since user[0] already
+          // has a token that we manually created in seed.js, if you test
+          // users[0] you would have to check the second element in the tokens
+          // array to see if it matches the x-auth header.
+          expect(user.tokens[1]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[0].email,
+        password: '1234567'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      })
   });
 });
